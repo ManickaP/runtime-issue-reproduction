bd020b0304c6805df3585e554c2b2848cd18b22e
ManickaP <mapichov@microsoft.com>
2025-11-28 13:38:47 +0100

    baa

--- program/Program.cs
+++ program/Program.cs
@@ -7,12 +7,11 @@ using System.Text;
 using System.Text.Unicode;
 
 var stream = new MemoryStream();
+
 await SseFormatter.WriteAsync<int>(GetItems(), stream, (item, writer) =>
 {
     writer.Write(Encoding.UTF8.GetBytes(item.Data.ToString()));
 });
-
-Console.WriteLine(Encoding.UTF8.GetString(stream.GetBuffer()));
 
 stream.Seek(0, SeekOrigin.Begin);
 
@@ -21,6 +20,7 @@ var parser = SseParser.Create(stream, (type, data) =>
     var str = Encoding.UTF8.GetString(data);
     return Int32.Parse(str);
 });
+
 await foreach (var item in parser.EnumerateAsync())
 {
     Console.WriteLine($"{item.EventType}: {item.Data} {item.EventId} {item.ReconnectionInterval} [{parser.LastEventId};{parser.ReconnectionInterval}]");
@@ -29,7 +29,7 @@ await foreach (var item in parser.EnumerateAsync())
 static async IAsyncEnumerable<SseItem<int>> GetItems()
 {
     yield return new SseItem<int>(1) { ReconnectionInterval = TimeSpan.FromSeconds(1) };
-    yield return new SseItem<int>(2);
+    yield return new SseItem<int>(2) { EventId = "2" };
     yield return new SseItem<int>(3);
     yield return new SseItem<int>(4);
 }
